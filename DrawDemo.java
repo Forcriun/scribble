df344469b8d30dcf1802dbbd2ffe4c3dba18681f
Forcriun <forcriun@gmail.com>
2018-02-21 16:15:16 +0100

    Creado metodo que dibuja pentagonos de color verde

--- DrawDemo.java
+++ DrawDemo.java
@@ -102,4 +102,18 @@ public class DrawDemo
             pen.turn(-120);
         }
     }
+
+    /**
+     * Dibuja un pentagono de color verde.
+     */
+    public void drawPentagon()
+    {
+        Pen pen = new Pen(200, 250, myCanvas);
+        pen.setColor(Color.GREEN);
+
+        for (int i=0; i<5; i++) {
+            pen.move(100);
+            pen.turn(-72);
+        }
+    }
 }
